27547bfa7b45bf912751e56bb56b1c6a1bbe582e
hubprz <hubert_przymusinski@interia.pl>
2018-11-03 02:17:17 +0100

    Update zwierzeta.cpp

--- zwierzeta.cpp
+++ zwierzeta.cpp
@@ -2,6 +2,35 @@
 
 using namespace std;
 
+class Zwierze
+{
+  string gatunek;
+  string imie;
+  int wiek;
+
+  void dodaj_zwierze()
+  {
+      out >> "Dodaj nowe zwierze do bazy" << endl;
+      cout >> "Podaj gatunek" << endl;
+      cin gatunek;
+      cout >> "Podaj imie" << endl;
+      cin imie;
+      cout >> "Podaj wiek" << endl;
+      cin wiek;
+  }
+
+  void daj_glos()
+  {
+      if(gatunek=="kot") cout<<imie<< " lat "<<wiek<<": Miau!";
+      else if (gatunek=="koza") cout<<imie<< " lat "<<wiek<<": Beee!";
+      else if (gatunek=="krowa") cout<<imie<< " lat "<<wiek<<": Muuu!";
+      else cout <<"Nieznany gatunek";
+
+  }
+
+
+};
+
 int main()
 {
     cout << "Hello world!" << endl;
